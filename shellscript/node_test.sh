9606543300ea976fa6746652946259d4e36e0345
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-26 03:40:39 +0000

    automate pub/sub pair testing

--- shellscript/node_test.sh
+++ shellscript/node_test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # specify which file to launch
-launch_file=launch_node_num_test.launch
+launch_file=auto_generated.launch
+node_num=5
 
 # build
 cd ~/ros1
@@ -13,14 +14,19 @@ cd ~/ros1
 gnome-terminal -- sh -c "roscore; bash"
 sleep 5s
 
-# launch
-cd src/ros1_test/launch
-sleep 1s
-#
 # for node_num in 1 20 40 60 80 100 120 140 160 180 200 220 240 260 280 300 320 340 360 380 400
 # do
-#     sed
-#     sleep 1s
+# 
+# generate launch file
+if [ 'auto_generated.launch' = ${launch_file} ] ; then
+    echo launch file auto-generated
+    . ~/ros1/src/ros1_test/shellscript/tools/launch_generator.sh $node_num
+fi
+# 
+# launch
+cd ~/ros1/src/ros1_test/launch
+sleep 1s
+#
 roslaunch $launch_file
 #     sleep 30s
 #     cd ~/Document
---
+++ shellscript/tools/launch_generator.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+node_num=$1
+
+# specify which launch file to edit
+cd ~/ros1/src/ros1_test/launch
+launch_file=auto_generated.launch
+
+# delete present
+sed -i '/^/d' $launch_file
+
+# generate
+echo "<launch>" >> $launch_file
+# publisher
+for i in `seq $node_num`
+do
+    echo "    <node pkg='ros1_test' name='delay_test_pub_$i' type='node_num_test_pub.py'>" >> $launch_file
+    echo "        <param name='node_num' value='$i' />" >> $launch_file
+    echo "    </node>" >> $launch_file
+done
+# subscriber
+for i in `seq $node_num`
+do
+    echo "    <node pkg='ros1_test' name='delay_test_sub_$i' type='node_num_test_sub.py'>" >> $launch_file
+    echo "        <param name='node_num' value='$i' />" >> $launch_file
+    echo "    </node>" >> $launch_file
+done
+# recorder
+# echo "    <node pkg='ros1_test' name='delay_test_pub' type='node_num_test_pub.py'>" >> $launch_file
+# echo "        <param name='node_num' value='$i'>" >> $launch_file
+# echo "    </node>" >> $launch_file
+
+
+echo "</launch>" >> $launch_file
