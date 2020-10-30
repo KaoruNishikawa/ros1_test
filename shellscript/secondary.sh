111da7050798ba0b528a1f8650ac29a111b92540
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-30 15:26:57 +0900

    bug fix

--- shellscript/secondary.sh
+++ shellscript/secondary.sh
@@ -5,11 +5,11 @@ export ROS_MASTER_URI=http://192.168.101.16:11311
 
 roscd ros1_test/launch
 
-#PUBLISHER
-timeout -s SIGINT 100s roslaunch launch_delay_pub.launch
+# PUBLISHER
+roslaunch launch_delay_pub.launch
 
 # SUBSCRIBER
-# roslaunch launch_delay_sub.launch
+# timeout -s SIGINT 100s roslaunch launch_delay_sub.launch
 # cd ~/Documents
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
