6ff10025809af1308b15e4ad7541af289ba44934
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-30 16:48:21 +0900

    bug fix

--- shellscript/host.sh
+++ shellscript/host.sh
@@ -6,37 +6,36 @@ sleep 15s
 roscd ros1_test/launch
 
 # PUBLISHER
-launch_file=launch_delay_pub.launch
-roslaunch $launch_file
+run_file=node_num_test_pub.py
+rosrun ros1_test $run_file
 
 # SUBSCRIBER
-# launch_file=launch_delay_sub.launch
-# timeout -s SIGINT 100s roslaunch $launch_file
+# run_file=node_num_test_sub.py
+# timeout -s SIGINT 100s rosrun ros1_test $run_file
 # cd ~/Documents
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
 # mv test_node_num* ./$dirname/data/
 # roscd ros1_test/shellscript
 # cp -r ./* ~/Documents/$dirname/
-# cp ../launch/$launch_file ~/Documents/$dirname/
+# cp ../ros1_test/$run_file ~/Documents/$dirname/
 # mkdir ~/Documents/$dirname/scripts
-# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
 # cp ../CMakeLists.txt ~/Documents/$dirname/
 # cp ../package.xml ~/Documents/$dirname/
 # touch ~/Documents/$dirname/settings.txt
 
 # INTRA-COMPUTER
-# launch_file=launch_delay_pubsub.launch
-# timeout -s SIGINT 100s roslaunch $launch_file
+# gnome-terminal -- sh -c "rosrun ros1_test node_num_test_pub.py"
+# run_file=node_num_test_sub.py
+# timeout -s SIGINT 100s rosrun ros1_test $run_file
 # cd ~/Documents
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
 # mv test_node_num* ./$dirname/data/
 # roscd ros1_test/shellscript
 # cp -r ./* ~/Documents/$dirname/
-# cp ../launch/$launch_file ~/Documents/$dirname/
+# cp ../ros1_test/$run_file ~/Documents/$dirname/
 # mkdir ~/Documents/$dirname/scripts
-# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
 # cp ../CMakeLists.txt ~/Documents/$dirname/
 # cp ../package.xml ~/Documents/$dirname/
 # touch ~/Documents/$dirname/settings.txt
--- shellscript/secondary.sh
+++ shellscript/secondary.sh
@@ -5,40 +5,40 @@
 export ROS_HOSTNAME=192.168.101.15
 export ROS_MASTER_URI=http://192.168.101.15:11311
 
+sleep 5s
 roscd ros1_test/launch
 
 # PUBLISHER
-launch_file=launch_delay_pub.launch
-roslaunch $launch_file
+# run_file=node_num_test_pub.py
+# rosrun ros1_test $run_file
 
 # SUBSCRIBER
-# launch_file=launch_delay_sub.launch
-# timeout -s SIGINT 100s roslaunch $launch_file
-# cd ~/Documents
-# dirname=result_$(date "+%Y%m%d_%H%M%S")
-# mkdir -p $dirname/data
-# mv test_node_num* ./$dirname/data/
-# roscd ros1_test/shellscript
-# cp -r ./* ~/Documents/$dirname/
-# cp ../launch/$launch_file ~/Documents/$dirname/
-# mkdir ~/Documents/$dirname/scripts
-# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
-# cp ../CMakeLists.txt ~/Documents/$dirname/
-# cp ../package.xml ~/Documents/$dirname/
-# touch ~/Documents/$dirname/settings.txt
+run_file=node_num_test_sub.py
+timeout -s SIGINT 100s rosrun ros1_test $run_file
+cd ~/Documents
+dirname=result_$(date "+%Y%m%d_%H%M%S")
+mkdir -p $dirname/data
+mv test_node_num* ./$dirname/data/
+roscd ros1_test/shellscript
+cp -r ./* ~/Documents/$dirname/
+cp ../ros1_test/$run_file ~/Documents/$dirname/
+mkdir ~/Documents/$dirname/scripts
+cp ../CMakeLists.txt ~/Documents/$dirname/
+cp ../package.xml ~/Documents/$dirname/
+touch ~/Documents/$dirname/settings.txt
 
 # INTRA-COMPUTER
-# launch_file=launch_delay_pubsub.launch
-# timeout -s SIGINT 100s roslaunch $launch_file
+# gnome-terminal -- sh -c "rosrun ros1_test node_num_test_pub.py"
+# run_file=node_num_test_sub.py
+# timeout -s SIGINT 100s rosrun ros1_test $run_file
 # cd ~/Documents
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
 # mv test_node_num* ./$dirname/data/
 # roscd ros1_test/shellscript
 # cp -r ./* ~/Documents/$dirname/
-# cp ../launch/$launch_file ~/Documents/$dirname/
+# cp ../ros1_test/$run_file ~/Documents/$dirname/
 # mkdir ~/Documents/$dirname/scripts
-# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
 # cp ../CMakeLists.txt ~/Documents/$dirname/
 # cp ../package.xml ~/Documents/$dirname/
 # touch ~/Documents/$dirname/settings.txt
