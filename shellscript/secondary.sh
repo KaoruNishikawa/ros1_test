eb878bf7a0fef0357513e19e4acbb3aad5a1ce43
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-30 15:46:40 +0900

    bug fix

--- shellscript/secondary.sh
+++ shellscript/secondary.sh
@@ -1,7 +1,9 @@
 # if you are 192.168.101.15
-export ROS_MASTER_URI=http://192.168.101.16:11311
+# export ROS_HOSTNAME=192.168.101.16
+# export ROS_MASTER_URI=http://192.168.101.16:11311
 # if you are 192.168.101.16
-# export ROS_MASTER_URI=http://192.168.101.15:11311
+export ROS_HOSTNAME=192.168.101.15
+export ROS_MASTER_URI=http://192.168.101.15:11311
 
 roscd ros1_test/launch
 
