b7055706c8b4b29a6fb6a4df4569d6089a3313e1
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-30 16:55:45 +0900

    add node_num-less node

---
+++ ros1_test/delay_pub.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+node_name = "delay_test_pub"
+
+import rospy
+import time
+from std_msgs.msg import Float64
+
+class node_num_test_pub(object):
+
+    def __init__(self):
+        # self.num = str(rospy.get_param('~node_num'))
+        self.my_pub = rospy.Publisher("/test/node_num_0", Float64, queue_size=1)
+        self.rate = rospy.Rate(10)
+
+    def my_publisher(self):
+        while not rospy.is_shutdown():
+            cur_time = time.time()
+            self.my_pub.publish(cur_time)
+            self.rate.sleep()
+
+
+def main(args=None):
+    rospy.init_node(node_name)
+    pub = node_num_test_pub()
+    pub.my_publisher()
+    # rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ ros1_test/delay_sub.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+node_name = "delay_test_sub"
+
+import rospy
+import time
+import os
+from std_msgs.msg import Float64
+
+class node_num_test_sub(object):
+
+    def __init__(self):
+        # self.num = str(rospy.get_param('~node_num'))
+        self.f = open(f"{os.environ['HOME']}/Documents/test_node_num_0.txt", "w")
+        my_sub = rospy.Subscriber("/test/node_num_0", Float64, self.sub_callback, queue_size=1)
+        rospy.spin()
+
+    def sub_callback(self, timer):
+        cur_time = time.time()
+        send_time = timer.data
+        delta = cur_time - send_time
+        self.f.write(str(delta)+'\n')
+
+
+def main(args=None):
+    rospy.init_node(node_name)
+    sub = node_num_test_sub()
+    # pub.my_publisher()
+    # rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
+
