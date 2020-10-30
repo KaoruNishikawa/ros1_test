1fd11710547026b53fcb480c9e47649233ffa0c4
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-30 14:51:36 +0900

    add some functionality

--- shellscript/host.sh
+++ shellscript/host.sh
@@ -1,2 +1,10 @@
 export ROS_MASTER_URI=http://localhost:11311
 gnome-terminal -- sh -c "roscore; bash"
+
+sleep 15s
+
+#PUBLISHER
+timeout -s SIGINT 100s roslaunch launch_delay_pub.launch
+
+# SUBSCRIBER
+# roslaunch launch_delay_sub.launch
--- shellscript/secondary.sh
+++ shellscript/secondary.sh
@@ -1,1 +1,13 @@
+# if you are 192.168.101.15
 export ROS_MASTER_URI=http://192.168.101.16:11311
+# if you are 192.168.101.16
+# export ROS_MASTER_URI=http://192.168.101.15:11311
+
+
+roscd ros1_test/launch
+
+#PUBLISHER
+timeout -s SIGINT 100s roslaunch launch_delay_pub.launch
+
+# SUBSCRIBER
+# roslaunch launch_delay_sub.launch
