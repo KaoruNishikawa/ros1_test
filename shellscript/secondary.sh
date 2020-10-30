e69adbc9a8050b2ff6146e517b8b8581bea5fc78
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-30 15:08:30 +0900

    add cleaning method

--- shellscript/secondary.sh
+++ shellscript/secondary.sh
@@ -11,6 +11,14 @@ timeout -s SIGINT 100s roslaunch launch_delay_pub.launch
 
 # SUBSCRIBER
 # roslaunch launch_delay_sub.launch
+# cd ~/Documents
+# dirname=result_$(date "+%Y%m%d_%H%M%S")
+# mkdir -p $dirname/data
+# mv test_node_num* ./$dirname/data/
 
 # INTRA-COMPUTER
 # roslaunch launch_delay_pubsub.launch
+# cd ~/Documents
+# dirname=result_$(date "+%Y%m%d_%H%M%S")
+# mkdir -p $dirname/data
+# mv test_node_num* ./$dirname/data/
