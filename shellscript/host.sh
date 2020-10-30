ae9ea0639f175fb46d8b23110fea634794337cd5
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-30 15:14:02 +0900

    add files to record

--- shellscript/host.sh
+++ shellscript/host.sh
@@ -3,6 +3,8 @@ gnome-terminal -- sh -c "roscore; bash"
 
 sleep 15s
 
+roscd ros1_test/launch
+
 #PUBLISHER
 timeout -s SIGINT 100s roslaunch launch_delay_pub.launch
 
@@ -12,10 +14,26 @@ timeout -s SIGINT 100s roslaunch launch_delay_pub.launch
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
 # mv test_node_num* ./$dirname/data/
+# roscd ros1_test/shellscript
+# cp -r ./* ~/Documents/$dirname/
+# cp ../launch/$launch_file ~/Documents/$dirname/
+# mkdir ~/Documents/$dirname/scripts
+# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
+# cp ../CMakeLists.txt ~/Documents/$dirname/
+# cp ../package.xml ~/Documents/$dirname/
+# touch ~/Documents/$dirname/settings.txt
 
 # INTRA-COMPUTER
-# roslaunch launch_delay_pubsub.launch
+# timeout -s SIGINT 100s roslaunch launch_delay_pubsub.launch
 # cd ~/Documents
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
 # mv test_node_num* ./$dirname/data/
+# roscd ros1_test/shellscript
+# cp -r ./* ~/Documents/$dirname/
+# cp ../launch/$launch_file ~/Documents/$dirname/
+# mkdir ~/Documents/$dirname/scripts
+# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
+# cp ../CMakeLists.txt ~/Documents/$dirname/
+# cp ../package.xml ~/Documents/$dirname/
+# touch ~/Documents/$dirname/settings.txt
--- shellscript/secondary.sh
+++ shellscript/secondary.sh
@@ -3,7 +3,6 @@ export ROS_MASTER_URI=http://192.168.101.16:11311
 # if you are 192.168.101.16
 # export ROS_MASTER_URI=http://192.168.101.15:11311
 
-
 roscd ros1_test/launch
 
 #PUBLISHER
@@ -15,10 +14,26 @@ timeout -s SIGINT 100s roslaunch launch_delay_pub.launch
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
 # mv test_node_num* ./$dirname/data/
+# roscd ros1_test/shellscript
+# cp -r ./* ~/Documents/$dirname/
+# cp ../launch/$launch_file ~/Documents/$dirname/
+# mkdir ~/Documents/$dirname/scripts
+# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
+# cp ../CMakeLists.txt ~/Documents/$dirname/
+# cp ../package.xml ~/Documents/$dirname/
+# touch ~/Documents/$dirname/settings.txt
 
 # INTRA-COMPUTER
-# roslaunch launch_delay_pubsub.launch
+# timeout -s SIGINT 100s roslaunch launch_delay_pubsub.launch
 # cd ~/Documents
 # dirname=result_$(date "+%Y%m%d_%H%M%S")
 # mkdir -p $dirname/data
 # mv test_node_num* ./$dirname/data/
+# roscd ros1_test/shellscript
+# cp -r ./* ~/Documents/$dirname/
+# cp ../launch/$launch_file ~/Documents/$dirname/
+# mkdir ~/Documents/$dirname/scripts
+# cp -r ../ros1_test/* ~/Documents/$dirname/scripts/
+# cp ../CMakeLists.txt ~/Documents/$dirname/
+# cp ../package.xml ~/Documents/$dirname/
+# touch ~/Documents/$dirname/settings.txt
