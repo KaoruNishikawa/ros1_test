7bedb5f28505649d4cf9bf1afaed9d121f596e78
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-14 18:15:26 +0900

    change data amount

--- ros1_test/delay_pub.py
+++ ros1_test/delay_pub.py
@@ -4,7 +4,7 @@ node_name = "delay_test_pub"
 
 import rospy
 import time
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, String
 
 class node_num_test_pub(object):
 
@@ -12,12 +12,17 @@ class node_num_test_pub(object):
         self.num = int(rospy.get_param('~node_num'))
         self.topic_num = str(rospy.get_param('~topic_num'))
         self.my_pub = rospy.Publisher(f"/test/node_num_{self.topic_num}", Float64, queue_size=1)
+        if self.num < 900:
+            self.data_pub = rospy.Publisher(f"/test/data_{self.num}", String, queue_size=1)
         self.rate = rospy.Rate(10)
 
     def my_publisher(self):
         while not rospy.is_shutdown():
             cur_time = time.time()
             self.my_pub.publish(cur_time)
+            if self.num < 900:
+                data = "a" * 1000
+                self.data_pub.publish(data)
             self.rate.sleep()
 
 
--- ros1_test/dummy_sub.py
+++ ros1_test/dummy_sub.py
@@ -5,19 +5,20 @@ node_name = "dummy_sub"
 import rospy
 import time
 import os
-from std_msgs.msg import Float64
+from std_msgs.msg import String
 
 class dummy_sub(object):
 
     def __init__(self):
         self.num = str(rospy.get_param('~node_num'))
-        my_sub = rospy.Subscriber("/test/node_num_"+self.num, Float64, self.sub_callback, queue_size=1)
+        my_sub = rospy.Subscriber("/test/node_num_"+self.num, String, self.sub_callback, queue_size=1)
         rospy.spin()
 
-    def sub_callback(self, timer):
-        cur_time = time.time()
-        send_time = timer.data
-        delta = cur_time - send_time
+    def sub_callback(self, data):
+        length = len(data)
+        # cur_time = time.time()
+        # send_time = timer.data
+        # delta = cur_time - send_time
 
 
 def main(args=None):
