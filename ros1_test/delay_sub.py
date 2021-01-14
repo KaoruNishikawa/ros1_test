af09e90f214f35f488ef116ee1e1c54b9b35ab88
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-14 15:18:11 +0900

    resolve name mismatch

--- ros1_test/delay_sub.py
+++ ros1_test/delay_sub.py
@@ -10,7 +10,7 @@ from std_msgs.msg import Float64
 class node_num_test_sub(object):
 
     def __init__(self):
-        self.num = str(rospy.get_param('~node_num'))
+        self.node_num = str(rospy.get_param('~node_num'))
         self.topic_num = str(rospy.get_param('~topic_num'))
         self.f = open(f"{os.environ['HOME']}/Documents/test_node_num_{self.node_num}.txt", "w")
         my_sub = rospy.Subscriber(f"/test/node_num_{self.topic_num}", Float64, self.sub_callback, queue_size=1)
