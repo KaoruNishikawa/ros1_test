0bdbe432d86a6801c3fe527ec9e3f7e044a3de26
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-06-23 03:01:11 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="ros1_test",
+    version="0.1.0",
+    packages=["ros1_test"],
+    data_files=[],
+    install_requires=["setuptools", "xarray-dataclasses >= 0.3", "ndradex >= 0.1"],
+    zip_safe=True,
+    maintainer="Kaoru Nishikawa",
+    maintainer_email="k.nishikawa@a.phys.nagoya-u.ac.jp",
+    description="",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={},
+)
